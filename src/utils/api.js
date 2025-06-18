79796a6344c691ac470790de0dfceab4cb01a19e
Shukurulla <shukurullatursinbayev978@gmai.com>
2025-06-18 11:08:35 +0500

    start

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Create axios instance
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://mental-server.vercel.app/",
+  baseURL:
+    import.meta.env.VITE_API_URL || "https://mental-server.vercel.app/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
